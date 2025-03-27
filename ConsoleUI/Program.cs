8458eda12651765e81d167c30b31fa0b06a4c16b
Altun98 <92326419+Altun98@users.noreply.github.com>
2025-03-27 22:41:15 +0400

    27.03.2025 9 -cu dersin sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,10 +9,39 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //  GetAllProduct();
+            //   GetAllCategory();
+            // GetAllProductDetails();
+            Console.ReadLine();
+        }
+
+        private static void GetAllProductDetails()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetByStock(10,18))
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine($"Mehsul: {item.ProductName} ---- Catagory: {item.CategoryName}");
+            }
+           
+        }
+
+        private static void GetAllProduct()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            Console.WriteLine("-------------------------------------------------");
+            foreach (var item in productManager.GetAll())
             {
                 Console.WriteLine($"Adi:{item.ProductName} - Qiymeti:{item.UnitPrice} - Sayi:{item.UnitsInStock}");
+            }
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        private static void GetAllCategory()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
             }
         }
     }
